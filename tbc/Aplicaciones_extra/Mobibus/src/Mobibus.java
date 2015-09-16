4804fe2a0e8f4c36615232fd219614cb1ca9be18
Jose Alejandro Ria¤o <ja.riano913@uniandes.edu.co>
2015-09-15 21:26:42 -0500

    Revert "Las aplicaciones stand alone"
    This reverts commit ffc9622468fe81ed7b4e4a9bbea0ef39a00856a6.

--- tbc/Aplicaciones_extra/Mobibus/src/Mobibus.java
+++ tbc/Aplicaciones_extra/Mobibus/src/Mobibus.java
@@ -21,31 +21,19 @@ public class Mobibus
 	//-----------------------------------------------
 	// Constantes
 	//----------------------------------------------
-	private final static String COMANDO_CAMBIAR_ESTADO="estado";
 	
-	private final static String COMANDO_CAMBIAR_POSICION="posicion";
+	private final static String URL_GET_LOCAL="http://127.0.0.1:3000/mobibuses.json";
 	
-	private final static String HTTP="http://";
+	private final static String URL_PUT_ESTADO_LOCAL="http://127.0.0.1:3000/mobibuses/1/estado.json";
 	
-	private final static String LOCALHOST_RAILS="127.0.0.1:3000";
+	private final static String URL_PUT_POSICION_LOCAL="http://127.0.0.1:3000/mobibuses/2/posicion.json";
 	
-	private final static String URL_GET ="/mobibuses.json";
+	private final static String ESTADO_EMERGENCIA ="EMERGENCIA";
 	
-	private final static String URL_PUT_ESTADO_PARTE_1="/mobibuses/";
-	
-	private final static String URL_PUT_ESTADO_PARTE_2="/estado.json";
-	
-	private final static String URL_PUT_POSICION_PARTE_1="/mobibuses/";
-	
-	private final static String URL_PUT_POSICION_PARTE_2="/posicion.json";
-	
-
+	private final static int POSICION =2424;
 	//-----------------------------------------------
 	// Atributos
 	//----------------------------------------------
-	
-	private String id;
-	
 	
 	//-----------------------------------------------
 	// Métodos
@@ -54,9 +42,9 @@ public class Mobibus
 	/**
 	 * Constructor de la clase.
 	 */
-	public Mobibus(String idP)
+	public Mobibus()
 	{
-		id=idP;
+		
 	}
 	
 	/**
@@ -66,7 +54,7 @@ public class Mobibus
 	private void enviarGet() throws Exception
 	{
 		
-		String url =  HTTP+LOCALHOST_RAILS+URL_GET;
+		String url =  URL_GET_LOCAL;
 		URL query= new URL(url);
 		HttpURLConnection conexion =  (HttpURLConnection) query.openConnection();
 		
@@ -94,6 +82,40 @@ public class Mobibus
 
 	
 	
+	/* HTTP GET request
+		private void sendGet() throws Exception 
+		{
+
+			String url = "http://www.google.com/search?q=mkyong";
+			
+			URL obj = new URL(url);
+			HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+			// optional default is GET
+			con.setRequestMethod("GET");
+
+			//add request header
+			con.setRequestProperty("User-Agent", USER_AGENT);
+
+			int responseCode = con.getResponseCode();
+			System.out.println("\nSending 'GET' request to URL : " + url);
+			System.out.println("Response Code : " + responseCode);
+
+			BufferedReader in = new BufferedReader(
+			        new InputStreamReader(con.getInputStream()));
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			in.close();
+
+			//print result
+			System.out.println(response.toString());
+
+		}*/
+	
 	/**
 	 * Envia una peticion PUT que cambia el estado del mobibus.
 	 * @param nuevoEstado El nuevo estado
@@ -102,10 +124,8 @@ public class Mobibus
 	private void cambiarEstado(String nuevoEstado) throws Exception
 	{
 		
-		String url =  HTTP + LOCALHOST_RAILS + URL_PUT_ESTADO_PARTE_1 + id + URL_PUT_ESTADO_PARTE_2;	
-		
-		//Lo que va antes del '=' debe ser el nombre del parámetro que espera la aplicación
-		String parametros = "estado="+nuevoEstado;
+		String url =  URL_PUT_ESTADO_LOCAL;	
+		String parametros = "estado="+ESTADO_EMERGENCIA;
 		byte[] postData   = parametros.getBytes( StandardCharsets.UTF_8 );
 		int    postDataLength = postData.length;
 		
@@ -152,10 +172,8 @@ public class Mobibus
 	 */
 	private void cambiarPosicion(String nuevaPosicion) throws Exception
 	{
-		String url =  HTTP + LOCALHOST_RAILS + URL_PUT_POSICION_PARTE_1 + id + URL_PUT_POSICION_PARTE_2;
-		
-		//Lo que va antes del '=' debe ser el nombre del parámetro que espera la aplicación
-		String parametros = "posicion="+nuevaPosicion;
+		String url =  URL_PUT_POSICION_LOCAL;	
+		String parametros = "posicion="+POSICION;
 		byte[] postData   = parametros.getBytes( StandardCharsets.UTF_8 );
 		int postDataLength = postData.length;
 		
@@ -206,31 +224,18 @@ public class Mobibus
 	public static void main (String[] args)
 	{
 		
-		//Deben haber exactamente tres parámetros: id, comando y nuevo valor.
-		if(args.length != 3)
-		{
-			System.err.println("Uso: java Mobibus <id> <comando> <nuevo Valor>");
-			System.out.println("comandos: " + COMANDO_CAMBIAR_ESTADO +"," + COMANDO_CAMBIAR_POSICION);
-			System.exit(1);
-		}
-		
-		//args[0] = id del vehículo
-		//args[1] = comando. 
-		//args[2] = el nuevo valor (Eje: EMERGENCIA, o 123123[es una posicion])
-		Mobibus instancia = new Mobibus(args[0]);
+		Mobibus instancia = new Mobibus();
 		
 		try 
 		{
-			if(args[1].equals(COMANDO_CAMBIAR_ESTADO)) 
-				instancia.cambiarEstado(args[2]);
-			
-			if(args[1].equals(COMANDO_CAMBIAR_POSICION)) 
-				instancia.cambiarPosicion(args[2]);
-			
-			
 			//Envia una peticion GET
 			//instancia.enviarGet();
-
+			
+			//Envia una peticion de cambio de estado  a EMERGENCIA.
+			//instancia.cambiarEstado(ESTADO_EMERGENCIA);
+			
+			//Envia una peticion de cambio de posicion
+			instancia.cambiarPosicion(""+POSICION);
 		}
 		catch (Exception e) 
 		{
